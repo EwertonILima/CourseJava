979424b0b6038c052808720cd3744925bfd405ba
Ewerton Inácio Lima <ewertoninaciolima@gmail.com>
2020-09-29 09:24:23 -0300

    Java Basic - Estrutura Condicional
    Examples and Exercises:
    
    if-else;
    operador de atribuição cumulativa;
    switch case;
    condição ternária

---
+++ Estrutura Condicional - Java Basic/Exemplo_5_1_Condicao_Ternaria.java
@@ -0,0 +1,20 @@
+package estrutura_condicional;
+
+import java.util.Scanner;
+
+public class Exemplo_5_1_Condicao_Ternaria {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		
+		double preco =  34.5;
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+		
+		System.out.println(desconto);
+		
+		sc.close();
+
+	}
+
+}
