4fabd3fd40a205cd335ba38ae45fb8802f4c81ed
Ewerton Inácio Lima <ewertoninaciolima@gmail.com>
2020-10-06 18:26:54 -0300

    Java Basic - Another basic topics
    Bitwise;
    String Functions;
    Creating Functions;

---
+++ Outros Topicos Basicos - Java Basic/Exe_Pratice_1_CriandoFuncoes.java
@@ -0,0 +1,32 @@
+package outrosTopicosBasicos;
+
+import java.util.Scanner;
+
+public class Exe_Pratice_1_CriandoFuncoes {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Insira duas notas: ");
+		double n1 = sc.nextDouble();
+		double n2 = sc.nextDouble();
+		
+		double media = calculoMedia(n1, n2);
+		
+		mostraMedia(media);
+		
+		sc.close();
+	}
+
+		public static double calculoMedia(double x, double y) {
+			
+			double aux = (x + y) / 2;
+			
+			return (double) aux;
+		}
+		
+		public static void mostraMedia(double value) {
+			System.out.println("A média é: " + value);
+		}
+}
