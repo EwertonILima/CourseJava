3d0cfef565f67bc9030b6301e201b8cb81bd45f1
EwertonILima <ewertoninaciolima@gmail.com>
2020-10-28 09:37:01 -0300

    vectors;
    for;
    
    Boxing/Unboxing
    
    for each;

---
+++ Estruturas Repetitivas - Java Basic/vectors/Example_2_forEach.java
@@ -0,0 +1,78 @@
+package vetores;
+
+public class Example_2_forEach {
+
+	public static void main(String[] args) {
+		
+		
+		String[] vectString = new String[] {"Maria","Bob", "Alex"};
+		
+		for (int i=0;i<vectString.length;i++) {
+			System.out.println(vectString[i]);
+		}
+		
+		System.out.println("------------String-------For Each:");
+		
+		for(String forEach : vectString) {
+			System.out.println(forEach);
+		}
+		
+		System.out.println();
+		System.out.println("--------------Character--------------");
+		Character[] vectChar = new Character[] {'Z','Y','X','W','V'};
+		
+		System.out.println("Boxing:");
+		char text = 'T';
+		Character boxingString = text;
+		System.out.println(boxingString);
+		System.out.println();
+		System.out.println("Unboxing:");
+		char text2 = boxingString;
+		System.out.println(text2);
+		System.out.println();
+		
+		System.out.println("For Each:");
+		for(Character forEach : vectChar) {
+			System.out.println(forEach);
+		}
+		
+		System.out.println();
+		System.out.println("--------------Integer--------------");
+		Integer[] vectInt = new Integer[] {1, 3, 5, 7, 9};
+		
+		System.out.println("Boxing:");
+		int n1 = 10;
+		Integer boxingInt = n1;
+		System.out.println(boxingInt);
+		System.out.println();
+		System.out.println("Unboxing:");
+		int n2 = boxingInt;
+		System.out.println(n2);
+		System.out.println();
+		
+		System.out.println("For Each:");
+		for(Integer forEach : vectInt) {
+			System.out.println(forEach);
+		}
+		
+		System.out.println();
+		System.out.println("---------------Double--------------");
+		Double[] vectDouble = new Double[] {1.0, 2.0, 3.0, 4.0, 5.0};
+		
+		System.out.println("Boxing:");
+		double n3 = 5.0;
+		Double boxingDouble = n3;
+		System.out.println(boxingDouble);
+		System.out.println();
+		System.out.println("Unboxing:");
+		double n4 = boxingDouble;
+		System.out.println(n4);
+		System.out.println();
+		
+		System.out.println("For Each:");
+		for(Double forEach : vectDouble) {
+			System.out.println(forEach);
+		}
+	}
+
+}
