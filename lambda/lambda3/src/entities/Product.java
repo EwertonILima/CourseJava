3b2c7a8a2828f66c42ce1cf4d01622d69db5d32c
EwertonILima <ewertoninaciolima@gmail.com>
2021-04-10 19:31:25 -0300

    Method reference non static

--- lambda/lambda3/src/entities/Product.java
+++ lambda/lambda3/src/entities/Product.java
@@ -25,11 +25,15 @@ public class Product {
 	public void setPrice(Double price) {
 		this.price = price;
 	}
-	
+
 	public static void staticProductConsumer(Product p) {
 		p.setPrice(p.getPrice() * 1.1);
 	}
-	
+
+	public void nonStaticProductConsumer() {
+		price = price * 1.1;
+	}
+
 	@Override
 	public String toString() {
 		return name + ", " + String.format("%.2f", price);
