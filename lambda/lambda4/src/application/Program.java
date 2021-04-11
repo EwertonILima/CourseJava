7fd67ba47bd8e3abadc4d616b21cc381434a28e8
EwertonILima <ewertoninaciolima@gmail.com>
2021-04-11 14:50:58 -0300

    Method reference static

--- lambda/lambda4/src/application/Program.java
+++ lambda/lambda4/src/application/Program.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import entities.Product;
-import util.ProductFunction;
 
 public class Program {
 
@@ -18,7 +17,7 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		List<String> names = list.stream().map(new ProductFunction()).collect(Collectors.toList());
+		List<String> names = list.stream().map(Product::staticProductFunction).collect(Collectors.toList());
 
 		names.forEach(System.out::println);
 
